7e1cd1af3025445f42916365c877482a90cc7910
Tom Lynch <twhlynch@gmail.com>
2023-05-15 16:36:29 +1000

    more files and buttons

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('Home').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+document.getElementById('Docs').addEventListener('click', () => {
+    window.location.href = 'docs.html';
+});
+document.getElementById('Stats').addEventListener('click', () => {
+    window.location.href = 'stats.html';
+});
+document.getElementById('Other-Tools').addEventListener('click', () => {
+    window.location.href = 'tools.html';
+});
+document.getElementById('editor').addEventListener('click', () => {
+    window.location.href = 'editor.html';
+});
