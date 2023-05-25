b9ccd4830eb6edc62cb45164d87e28985de52e0b
index <81807841+twhlynch@users.noreply.github.com>
2023-05-25 12:55:16 +1000

    Update Records.py

--- Records.py
+++ Records.py
@@ -8,7 +8,10 @@ i = 1
 for level in data:
     id = level["identifier"].replace(":", "/")
     url = f"https://api.slin.dev/grab/v1/statistics_top_leaderboard/{id}"
-    res_data = requests.get(url).json()
+    try:
+        res_data = requests.get(url).json()
+    except:
+        pass
     if len(res_data) != 0:
         if res_data[0]["user_name"] not in leaderboard:
             leaderboard[res_data[0]["user_name"]] = [0, []]
