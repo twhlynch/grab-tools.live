86a6bfeb903eaa37612e25c63048ffa26b4381a2
Tom Lynch <twhlynch@gmail.com>
2023-06-26 13:18:48 +1000

    beat me

--- Records.py
+++ Records.py
@@ -14,9 +14,12 @@ for level in data:
         pass
     if len(res_data) != 0:
         if res_data[0]["user_name"] not in leaderboard:
-            leaderboard[res_data[0]["user_name"]] = [0, []]
+            leaderboard[res_data[0]["user_name"]] = [0, [], 0]
         leaderboard[res_data[0]["user_name"]][0] += 1
         leaderboard[res_data[0]["user_name"]][1].append([level["title"]])
+        for k in res_data:
+            if res_data[k]["user_name"] == "NSKC7":
+                leaderboard[res_data[0]["user_name"]][2] += 1
     print(i)
     i += 1
 
