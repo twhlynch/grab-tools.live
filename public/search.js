332dfe4c66a1f2b8c3423869d142abee59284e98
Tom Lynch <twhlynch@gmail.com>
2023-08-21 11:32:45 +1000

    :{

--- public/search.js
+++ public/search.js
@@ -8,47 +8,59 @@ submit.addEventListener("click", function (event) {
         let title = document.getElementById("search-title");
         let description = document.getElementById("search-description");
         let creators = document.getElementById("search-creators");
-        fetch('stats_data/all_levels1.json')
-        .then((response) => response.json())
-        .then(data => {
-            data.forEach(item => {
-                if (title.value == 'on') {
-                    if (item.title.includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+        fetch("stats_data/all_levels1.json")
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((item) => {
+                    if (title.checked) {
+                        try {
+                            if (item.title.includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
-                if (description.value == 'on') {
-                    if (item.description.includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+                    if (description.checked) {
+                        try {
+                            if (item.description.includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
-                if (creators.value == 'on') {
-                    if (item.creators.join().includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+                    if (creators.checked) {
+                        try {
+                            if (item.creators.join().includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
+                });
             });
-        });
-        fetch('stats_data/all_levels2.json')
-        .then((response) => response.json())
-        .then(data => {
-            data.forEach(item => {
-                if (title.value == 'on') {
-                    if (item.title.includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+        fetch("stats_data/all_levels2.json")
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((item) => {
+                    if (title.checked) {
+                        try {
+                            if (item.title.includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
-                if (description.value == 'on') {
-                    if (item.description.includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+                    if (description.checked) {
+                        try {
+                            if (item.description.includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
-                if (creators.value == 'on') {
-                    if (item.creators.join().includes(search)) {
-                        output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`
+                    if (creators.checked) {
+                        try {
+                            if (item.creators.join().includes(search)) {
+                                output.innerHTML += `<div class="search-item"><a href="https://grabvr.quest/levels/viewer/?level=${item["identifier"]}">${item["title"]}</a></div>`;
+                            }
+                        } catch {}
                     }
-                }
+                });
             });
-        });
     }
 });
