11f37bd40b2c882d3bc19626879e015addce835c
Tom Lynch <twhlynch@gmail.com>
2023-10-16 11:26:54 +1100

    wiki/nav

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-document.getElementById('Home').addEventListener('click', () => {
-    window.location.href = 'index.html';
-});
-document.getElementById('List').addEventListener('click', () => {
-    window.location.href = 'list.html';
-});
-document.getElementById('Stats').addEventListener('click', () => {
-    window.location.href = 'stats.html';
-});
-document.getElementById('Other-Tools').addEventListener('click', () => {
-    window.location.href = 'tools.html';
-});
+// document.getElementById('Home').addEventListener('click', () => {
+//     window.location.href = 'index.html';
+// });
+// document.getElementById('List').addEventListener('click', () => {
+//     window.location.href = 'list.html';
+// });
+// document.getElementById('Stats').addEventListener('click', () => {
+//     window.location.href = 'stats.html';
+// });
+// document.getElementById('Other-Tools').addEventListener('click', () => {
+//     window.location.href = 'tools.html';
+// });
 if (window.location.href.includes('index.html') || document.title == 'Grab Tools | .index | twhlynch') {
     document.getElementById('editor').addEventListener('click', () => {
         window.location.href = 'editor.html';
