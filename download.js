f78a2a86ea6305c61b1d261dd6d5fa2d7539dd9e
Tom Lynch <twhlynch@gmail.com>
2023-11-06 13:36:03 +1100

    download page

---
+++ download.js
@@ -0,0 +1,22 @@
+async function download(id) {
+    let SERVER_URL = 'https://api.slin.dev/grab/v1/';
+    let response = await fetch(SERVER_URL + 'details/' + id.replace(':', '/'));
+    let details = await response.json();
+    let iteration = await details.iteration;
+    let link = SERVER_URL + 'download/' + id.replace(':', '/') + '/' + iteration;
+  
+    let fileResponse = await fetch(link);
+    let fileBlob = await fileResponse.blob();
+    let url = window.URL.createObjectURL(fileBlob);
+    let a = document.createElement('a');
+    a.href = url;
+    a.download = id.split(":")[1] + '.level';
+    a.click();
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const level = urlParams.get('level');
+if (level) {
+    download(level);
+}
+
