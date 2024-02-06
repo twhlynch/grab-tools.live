2b58ccec31d2f265f5c25e73e22dc2ed51be6854
twhlynch <twhlynch@gmail.com>
2024-02-06 13:44:44 +1100

    login state

--- login.js
+++ login.js
@@ -13,12 +13,33 @@ async function login() {
                 let userResponse = await fetch('https://graph.oculus.com/me?access_token=' + oauth_token + '&fields=alias')
                 let userData = await userResponse.json();
                 if (userData) {
-                    localStorage.setItem('user', userData.alias);
-                    document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userData.alias);
-                    console.log(userData.alias);
+
+                    const usersDataResponse = await fetch(`https://api.slin.dev/grab/v1/list?type=user_name&search_term=${userData.alias}`);
+                    const usersData = await usersDataResponse.json();
+
+                    let userId = null;
+
+                    if (usersData.length > 0) {
+                        usersData.forEach((item) => {
+                            if (!userId && item.user_name == userData.alias) {
+                                userId = item.user_id;
+                            }
+                        });
+                    }
+
+                    if (userId) {
+                        localStorage.setItem('userName', userData.alias);
+                        localStorage.setItem('userId', userId);
+                        document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userData.alias);
+                    }
+
                 }
             }
         }
+    } else if (localStorage.getItem('userId') != null) {
+        let userId = localStorage.getItem('userId');
+        let userName = localStorage.getItem('userName');
+        document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userName);
     }
 }
 
