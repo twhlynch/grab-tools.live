cdc5593b2074e34120faf238c6c5c1b54b26689c
index <81807841+twhlynch@users.noreply.github.com>
2024-02-15 14:32:29 +1100

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,17 +1,17 @@
-if (false && !localStorage.getItem("finalVotePopupShown")) {
+if (!localStorage.getItem("discordInviteShown")) {
     let voteOverlayContainer = document.createElement("div");
     voteOverlayContainer.setAttribute("id", "voteOverlayContainer");
 
     let voteOverlayText = document.createElement("p");
-    voteOverlayText.innerHTML = "Final voting for The GRAB Awards is open! Go cast your final votes!";
+    voteOverlayText.innerHTML = "The GRAB Tools Discord server is a great place to get help using the available tools and find out about tools you didn't know about! Consider joining with the link below or by using the invite code: YKfGWSYAqf";
 
     let yButton = document.createElement("a");
     yButton.classList.add("button");
-    yButton.innerHTML = "Vote Now!";
-    yButton.setAttribute("href", "https://grab-tools.live/vote");
+    yButton.innerHTML = "Join Now!";
+    yButton.setAttribute("href", "https://grab-tools.live/discord");
     yButton.addEventListener("click", () => {
         voteOverlayContainer.style.display = "none";
-        localStorage.setItem("finalVotePopupShown", "true");
+        localStorage.setItem("discordInviteShown", "true");
     });
 
     let lButton = document.createElement("button");
@@ -26,7 +26,7 @@ if (false && !localStorage.getItem("finalVotePopupShown")) {
     nButton.innerHTML = "No Thanks";
     nButton.addEventListener("click", () => {
         voteOverlayContainer.style.display = "none";
-        localStorage.setItem("finalVotePopupShown", "true");
+        localStorage.setItem("discordInviteShown", "true");
     });
 
     document.body.appendChild(voteOverlayContainer);
