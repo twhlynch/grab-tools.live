b48c02f568676e1370d86a183c0f56343d2eca75
Tom Lynch <twhlynch@gmail.com>
2024-03-11 18:39:52 +1100

    ghl list

--- js/list.js
+++ js/list.js
@@ -1,24 +1,23 @@
-fetch("/stats_data/list.json")
-.then(response => response.json())
-.then(data => {
-    if (data.judges.length > 0) {
-        const list = document.getElementById('list');
-        const judges = document.getElementById('judges');
-        const dataElement = document.getElementById('data');
-        for (let i = 0; i < data.levels.length; i++) {
-            let item = data.levels[i];
-            let {level, identifier, title, creator} = item.level;
-            let mapLink = `https://grabvr.quest/levels/viewer/?level=${identifier}`;
-            let userLink = `https://grabvr.quest/levels?tab=tab_other_user&user_id=${identifier.split(":")[0]}`;
+list = document.getElementById('list');
 
-            list.innerHTML += `
-            <div class="leaderboard-item">
-                <a href="${mapLink}">${title}</a><a href="${userLink}">${creator}</a>
-            </div>
-            `;
-        }
-        judges.innerHTML = data.judges.join('&nbsp;•&nbsp;');
-        dataElement.style.display = 'flex';
-        dataElement.style.flexDirection = 'column';
+fetch("/stats_data/list.csv")
+.then(response => response.text())
+.then(data => {
+
+    let rows = data.split('\n');
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i].split(';');
+
+        let title = row[0];
+        let creator = row[1];
+
+        list.innerHTML += `
+        <div class="leaderboard-item list-item">
+            <p>${i+1}</p>
+            <p>${title}</p>
+            <p>${creator}</p>
+        </div>
+        `;
     }
+
 });
