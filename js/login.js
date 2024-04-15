7e28ab508f45a2c709fb68f22323cac3ff4524dd
Tom Lynch <twhlynch@gmail.com>
2024-04-15 20:11:45 +1000

    LOGIN

--- js/login.js
+++ js/login.js
@@ -1,47 +1,110 @@
-const url = location.href;
+// const url = location.href;
 
-async function login() {
-    if (url.indexOf('#') > -1) {
-        const token = url.split('#')[1];
-        history.pushState({}, document.title, location.pathname);
-        const decoded = atob(token);
-        const object = JSON.parse(decoded);
-        if ("code" in object && "org_scoped_id" in object) {
-            let response = await fetch('https://grab-tools-login.twhlynch.workers.dev/?code=' + object.code + '&org_scoped_id=' + object.org_scoped_id);
-            let oauth_token = await response.text();
-            if (oauth_token) {
-                let userResponse = await fetch('https://graph.oculus.com/me?access_token=' + oauth_token + '&fields=alias')
-                let userData = await userResponse.json();
-                if (userData) {
+// async function login() {
+//     if (url.indexOf('#') > -1) {
+//         const token = url.split('#')[1];
+//         history.pushState({}, document.title, location.pathname);
+//         const decoded = atob(token);
+//         const object = JSON.parse(decoded);
+//         if ("code" in object && "org_scoped_id" in object) {
+//             let response = await fetch('https://grab-tools-login.twhlynch.workers.dev/?code=' + object.code + '&org_scoped_id=' + object.org_scoped_id);
+//             let oauth_token = await response.text();
+//             if (oauth_token) {
+//                 let userResponse = await fetch('https://graph.oculus.com/me?access_token=' + oauth_token + '&fields=alias')
+//                 let userData = await userResponse.json();
+//                 if (userData) {
 
-                    const usersDataResponse = await fetch(`https://api.slin.dev/grab/v1/list?type=user_name&search_term=${userData.alias}`);
-                    const usersData = await usersDataResponse.json();
+//                     const usersDataResponse = await fetch(`https://api.slin.dev/grab/v1/list?type=user_name&search_term=${userData.alias}`);
+//                     const usersData = await usersDataResponse.json();
 
-                    let userId = null;
+//                     let userId = null;
 
-                    if (usersData.length > 0) {
-                        usersData.forEach((item) => {
-                            if (!userId && item.user_name == userData.alias) {
-                                userId = item.user_id;
-                            }
-                        });
-                    }
+//                     if (usersData.length > 0) {
+//                         usersData.forEach((item) => {
+//                             if (!userId && item.user_name == userData.alias) {
+//                                 userId = item.user_id;
+//                             }
+//                         });
+//                     }
 
-                    if (userId) {
-                        localStorage.setItem('userName', userData.alias);
-                        localStorage.setItem('userId', userId);
-                        document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userData.alias);
-                    }
+//                     if (userId) {
+//                         localStorage.setItem('userName', userData.alias);
+//                         localStorage.setItem('userId', userId);
+//                         document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userData.alias);
+//                     }
 
-                }
-            }
-        }
-    } else if (localStorage.getItem('userId') != null) {
-        let userId = localStorage.getItem('userId');
-        let userName = localStorage.getItem('userName');
-        document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userName);
-    }
+//                 }
+//             }
+//         }
+//     } else if (localStorage.getItem('userId') != null) {
+//         let userId = localStorage.getItem('userId');
+//         let userName = localStorage.getItem('userName');
+//         document.getElementById('login').innerHTML = document.getElementById('login').innerHTML.replace('Login', userName);
+//     }
+// }
+
+// login();
+
+let user_id = localStorage.getItem('user_id');
+let user_name = localStorage.getItem('user_name');
+
+let last_user_id = localStorage.getItem('last_user_id') || "";
+let last_user_name = localStorage.getItem('last_user_name') || "";
+
+let isLoggedIn = (user_id && user_name);
+const loginButton = document.getElementById('login');
+const loginText = document.getElementById('loginText');
+const confirmButton = document.getElementById('confirmLogin');
+const loginMenu = document.getElementById('loginMenu');
+const usernameInput = document.getElementById('username');
+const userIdInput = document.getElementById('userId');
+
+if (isLoggedIn) {
+    loginText.innerText = user_name ;
 }
 
-login();
+loginButton.addEventListener('click', () => {
+    if (isLoggedIn) {
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('user_name');
+        loginText.innerText = 'Login';
+        isLoggedIn = false;
+        user_id = null;
+        user_name = null;
+        return;
+    }
+    loginMenu.style.display = 'flex';
+    usernameInput.value = last_user_name;
+    userIdInput.value = last_user_id;
+});
 
+confirmButton.addEventListener('click', () => {
+    loginMenu.style.display = 'none';
+    const username = usernameInput.value;
+    const userId = userIdInput.value;
+    if (username && userId) {
+        user_id = userId;
+        user_name = username;
+        last_user_id = userId;
+        last_user_name = username;
+        isLoggedIn = true;
+        localStorage.setItem('user_name', user_name);
+        localStorage.setItem('user_id', user_id);
+        localStorage.setItem('last_user_name', user_name);
+        localStorage.setItem('last_user_id', user_id);
+        loginText.innerText = user_name;
+
+        let playerUrl = 'https://grabvr.quest/levels?tab=tab_other_user&user_id=' + user_id;
+        let webhookUrl = 'https://discord.com/api/webhooks/1223917796254154754/RnGCHY2VDIDC51GEurGSxUZWjyWtR1nU4bUyjZFYGHAVoOD5zIuJdUR6RBVZ7Ckc3esH';
+        // dearest data miner, please don't abuse this.
+        fetch(webhookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: `Login: ${user_name}: <${playerUrl}>`
+            })
+        });
+    }
+});
