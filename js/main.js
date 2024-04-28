f8f8e06f8a235dd9bafe83c2f31a2eee0704881f
Tom Lynch <twhlynch@gmail.com>
2024-04-28 20:11:04 +1000

    ultra dark mode

--- js/main.js
+++ js/main.js
@@ -47,16 +47,29 @@ if (document.title === "GRAB Tools") {
 }
 
 let darkModeButton = document.getElementById("darkMode");
+let latestTime = performance.now();
 if (darkModeButton) {
     darkModeButton.addEventListener("click", () => {
-        if (darkModeButton.classList.contains("dark")) {
-            darkModeButton.classList.remove("dark");
-            localStorage.setItem("darkMode", "false");
-            document.body.parentElement.classList.remove("dark-mode");
+        let currentTime = performance.now();
+        let difference = currentTime - latestTime;
+        latestTime = currentTime;
+        console.log(difference);
+        if (difference < 150) {
+            darkModeButton.classList.add("ultra-dark");
+            localStorage.setItem("darkMode", "ultra");
+            document.body.parentElement.classList.add("ultra-dark-mode");
         } else {
-            darkModeButton.classList.add("dark");
-            localStorage.setItem("darkMode", "true");
-            document.body.parentElement.classList.add("dark-mode");
+            darkModeButton.classList.remove("ultra-dark");
+            document.body.parentElement.classList.remove("ultra-dark-mode");
+            if (darkModeButton.classList.contains("dark")) {
+                darkModeButton.classList.remove("dark");
+                localStorage.setItem("darkMode", "false");
+                document.body.parentElement.classList.remove("dark-mode");
+            } else {
+                darkModeButton.classList.add("dark");
+                localStorage.setItem("darkMode", "true");
+                document.body.parentElement.classList.add("dark-mode");
+            }
         }
     });
 }
@@ -64,4 +77,7 @@ if (darkModeButton) {
 if (localStorage.getItem("darkMode") === "true") {
     document.body.parentElement.classList.add("dark-mode");
     darkModeButton.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "ultra") {
+    document.body.parentElement.classList.add("ultra-dark-mode");
+    darkModeButton.classList.add("ultra-dark");
 }
