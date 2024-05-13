715029a950896f67b03deba8fe45ed112779d059
index <81807841+twhlynch@users.noreply.github.com>
2024-05-13 15:25:09 +1000

    Update getStats.py

--- getStats.py
+++ getStats.py
@@ -646,7 +646,8 @@ def run_bot(daily, unbeaten, weekly, unbeaten_levels=[], beaten_unbeaten_levels=
                 creator = map["creators"][0]
             unverified_embed = Embed(title=map["title"], url=f"{VIEWER_URL}?level={map['identifier']}", description=creator, color=0xff0000)
             if "images" in map and "thumb" in map["images"] and "key" in map["images"]["thumb"]:
-                unverified_embed.set_thumbnail(f"https://grab-images.slin.dev/{map["images"]["thumb"]["key"]}")
+                link = map["images"]["thumb"]["key"]
+                unverified_embed.set_thumbnail(f"https://grab-images.slin.dev/{link}")
             await unverified_channel.send(embed=unverified_embed)
             
         await bot.close()
