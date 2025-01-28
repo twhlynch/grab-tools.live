e4af179fe355e227a5084ac99d82bf1b4f319c7f
Tom Lynch <twhlynch@gmail.com>
2025-01-28 11:26:40 +1100

    more stuff

--- emails.sh
+++ emails.sh
@@ -3,6 +3,8 @@
 hash_file="$1"
 new_email="$2"
 
+temp_hash_file=$(mktemp)
+
 while IFS= read -r commit_hash; do
 	if git cat-file -e "$commit_hash^{commit}"; then
 
@@ -11,6 +13,9 @@ while IFS= read -r commit_hash; do
 		git checkout "$commit_hash"
 		git commit --amend --author="$current_author <$new_email>" --no-edit
 
+		new_commit_hash=$(git rev-parse HEAD)
+		echo "$commit_hash $new_commit_hash" >> "$temp_hash_file"
+
 		echo "Updated commit $commit_hash with new email: $new_email"
 	else
 		echo "Commit $commit_hash does not exist."
@@ -18,5 +23,15 @@ while IFS= read -r commit_hash; do
 done < "$hash_file"
 
 git checkout -
+
+while IFS= read -r line; do
+	old_hash=$(echo "$line" | awk '{print $1}')
+	new_hash=$(echo "$line" | awk '{print $2}')
+	
+	git replace "$old_hash" "$new_hash"
+	echo "Replaced $old_hash with $new_hash"
+done < "$temp_hash_file"
+
+rm "$temp_hash_file"
 
 echo "Success?"
