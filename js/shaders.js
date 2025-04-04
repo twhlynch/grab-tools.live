abd35b2f234f53f0af792802ede1bf3fd2adb756
Tom Lynch <twhlynch@gmail.com>
2025-04-04 21:20:06 +1100

    particles and sublevels

--- js/shaders.js
+++ js/shaders.js
@@ -290,3 +290,61 @@ export const signFS = `
 		gl_FragColor = color;
 		#include <colorspace_fragment>
 	}`
+export const particleVS = `
+	attribute vec3 color;
+	uniform float scale;
+
+	varying vec3 vColor;
+	varying vec3 vWorldPosition;
+
+	void main() 
+	{
+		vColor = color;
+
+		vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+		vWorldPosition = worldPosition.xyz;
+
+		vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+		gl_PointSize = scale * (300.0 / length(mvPosition.xyz));
+		gl_Position = projectionMatrix * mvPosition;
+	}`
+export const particleFS = `
+	varying vec3 vColor;
+	varying vec3 vWorldPosition;
+
+	uniform float fogEnabled;
+	uniform vec2 cameraFogDistance;
+	uniform vec3 cameraFogColor0;
+	uniform vec3 cameraFogColor1;
+	uniform float sunSize;
+	uniform vec3 sunColor;
+	uniform vec3 sunDirection;
+
+	void main()
+	{
+		vec4 color = vec4(vColor, 1.0);
+
+		vec3 cameraToVertex = vWorldPosition - cameraPosition;
+		float distanceToCamera = length(cameraToVertex);
+		cameraToVertex = normalize(cameraToVertex);
+
+		if(fogEnabled > 0.5)
+		{
+			float horizonFactor = 1.0 - clamp(abs(cameraToVertex.y) / 0.8, 0.0, 1.0);
+			vec3 fogColor = mix(cameraFogColor1.rgb, cameraFogColor0.rgb, horizonFactor * horizonFactor);
+
+			float sunAngle = acos(dot(sunDirection, -cameraToVertex));
+			float sunSize_ = 0.05 * sunSize;
+			float sunGlowSize = sunSize;
+			float sunFactor = clamp((sunGlowSize - sunAngle) / sunGlowSize, 0.0, 1.0);
+			sunFactor *= sunFactor;
+			fogColor = mix(fogColor, sunColor, sunFactor);
+
+			float fogAmount = clamp((1.0 - exp(-distanceToCamera * cameraFogDistance.x)) * cameraFogDistance.y, 0.0, 1.0);
+			color.rgb = mix(color.rgb, fogColor, fogAmount * fogAmount);
+		}
+
+		gl_FragColor = color;
+		#include <colorspace_fragment>
+	}
+`
