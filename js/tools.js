44e74b8b0f8e7bac69188f4bf35604d3c4279cc2
Tom Lynch <twhlynch@gmail.com>
2025-06-08 03:50:26 +1000

    and remove finishes

--- js/tools.js
+++ js/tools.js
@@ -18,7 +18,7 @@ function readArrayBufferGroup(file) {
                         "rotation": {
                             "w": 1.0
                         }, 
-                        "childNodes": object.levelNodes.filter(n => !(n.levelNodeStart)),
+                        "childNodes": object.levelNodes.filter(n => !(n.levelNodeStart) && !(n.levelNodeFinish)),
                         "scale": {
                             "y": 1.0, 
                             "x": 1.0, 
