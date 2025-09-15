fe44b0058fc0bbe988d0a0eaf7f82c4a9c6b1c53
Tom Lynch <twhlynch@gmail.com>
2025-09-15 21:09:32 +1000

    move "Tipping" to "statistics"

--- statistics.py
+++ statistics.py
@@ -1,10 +1,11 @@
-import json, requests, contextlib
+import json
+import requests
 
-with open("stats_data/all_verified.json") as file:
-    data = json.load(file)
 
 def get_level_stats(level_identifier):
-    stats_url = f"https://api.slin.dev/grab/v1/statistics/{level_identifier.replace(':', '/')}"
+    stats_url = (
+        f"https://api.slin.dev/grab/v1/statistics/{level_identifier.replace(':', '/')}"
+    )
     print(stats_url)
     stats_request = requests.get(stats_url)
     if stats_request.status_code == 200:
@@ -14,13 +15,18 @@ def get_level_stats(level_identifier):
         print("ERROR: INVALID RESPONSE FROM SERVER")
         return {}
 
+
 statistics = {}
+
+with open("stats_data/all_verified.json") as file:
+    data = json.load(file)
+
 for level in data:
     identifier = level["identifier"]
     try:
         stats = get_level_stats(identifier)
         statistics[identifier] = stats
-    except:
+    except Exception:
         continue
 
 with open("stats_data/statistics.json", "w") as file:
