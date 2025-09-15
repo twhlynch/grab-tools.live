093514eb66141e1158d96854516c70a645ba5995
Tom Lynch <twhlynch@gmail.com>
2025-09-15 21:05:18 +1000

    format Records.py

--- Records.py
+++ Records.py
@@ -3,66 +3,8 @@ import requests
 import contextlib
 from concurrent.futures import ThreadPoolExecutor
 
-def process_level(level):
-    identifier = level["identifier"].replace(":", "/")
-    url = f"https://api.slin.dev/grab/v1/statistics_top_leaderboard/{identifier}"
-    res_data = []
-    with contextlib.suppress(Exception):
-        res_data = requests.get(url).json()
-    
-    if len(res_data) != 0:
-        res_data = [entry for entry in res_data if "is_verification" not in entry]
-        print(f"Parsing leaderboard of {len(res_data)} results for {level['title']}")
-        level["leaderboard"] = res_data
-        if len(res_data) == 1:
-            sole_victors.append(level)
-        if len(res_data) > 0:
-            keyName = res_data[0]["user_id"]
-            if keyName not in leaderboard:
-                leaderboard[keyName] = [0, [], res_data[0]["user_name"]]
-            leaderboard[keyName][0] += 1
-            leaderboard[keyName][1].append([level["title"] + "|" + level["identifier"]])
-        level_diff = "unrated"
-        if "statistics" in level:
-            if "difficulty_string" in level["statistics"]:
-                level_diff = level["statistics"]["difficulty_string"]
-        difficulty_lengths[level_diff] += 1
-        for record in res_data:
-            if "timestamp" in record:
-                timestamps_data.append(f"{record["user_id"] if "user_id" in record else ""}:{int(int(record["timestamp"]) / 100)}")
-            if record["user_id"] not in difficulty_records[level_diff]:
-                difficulty_records[level_diff][record["user_id"]] = {
-                    "maps": 0, 
-                    "user_name": record["user_name"]
-                }
-            difficulty_records[level_diff][record["user_id"]]["maps"] += 1
-    
-    first_record = None
-    first_timestamp = 9999999999999
-    for record in res_data:
-        if int(record["timestamp"]) < first_timestamp:
-            first_timestamp = int(record["timestamp"])
-            first_record = record
-        if record["user_id"] not in user_finishes:
-            user_finishes[record["user_id"]] = [0, record["user_name"], 0]
-        user_finishes[record["user_id"]][0] += 1
-        try:
-            user_finishes[record["user_id"]][2] += record["best_time"] if "best_time" in record else 0
-        except TypeError:
-            print(user_finishes[record["user_id"]])
-        
-    if first_record is not None:
-        if first_record["user_id"] not in first_to_beat:
-            first_to_beat[first_record["user_id"]] = [
-                first_record["user_name"], 
-                0
-            ]
-        first_to_beat[first_record["user_id"]][1] += 1
-    
-    print(f"Processed {level['title']}")
 
-with open("stats_data/all_verified.json") as file:
-    data = json.load(file)
+API = "https://api.slin.dev/grab/v1/"
 
 difficulty_records = {
     "unrated": {},
@@ -70,7 +12,7 @@ difficulty_records = {
     "medium": {},
     "hard": {},
     "veryhard": {},
-    "impossible": {}
+    "impossible": {},
 }
 difficulty_lengths = {
     "unrated": 0,
@@ -79,58 +21,190 @@ difficulty_lengths = {
     "hard": 0,
     "veryhard": 0,
     "impossible": 0,
-    "total": 0
+    "total": 0,
 }
 leaderboard = {}
 sole_victors = []
 user_finishes = {}
 first_to_beat = {}
 timestamps_data = []
-difficulty_lengths["total"] = len(data)
 
+
+def get_leaderboard(identifier):
+    identifier = identifier.replace(":", "/")
+
+    url = f"{API}statistics_top_leaderboard/{identifier}"
+
+    leaderboard = []
+    with contextlib.suppress(Exception):
+        leaderboard = requests.get(url).json()
+
+    filtered_leaderboard = [
+        entry for entry in leaderboard if "is_verification" not in entry
+    ]
+
+    length = len(filtered_leaderboard)
+    print(f"{length} entries for {identifier}")
+
+    return filtered_leaderboard
+
+
+def save(filepath, data):
+    print(f"Saving {filepath}")
+
+    with open(f"stats_data/{filepath}.json", "w") as file:
+        json.dump(data, file)
+
+
+def process_level(level):
+    title = level["title"] if "title" in level else ""
+    identifier = level["identifier"] if "identifier" in level else None
+    statistics = level["statistics"] if "statistics" in level else {}
+
+    if identifier is None:
+        return
+
+    print(f"Processing {title}")
+
+    leaderboard_data = get_leaderboard(identifier)
+
+    length = len(leaderboard_data)
+
+    if length != 0:
+        level["leaderboard"] = leaderboard_data
+
+        # sole = only 1 record
+        if length == 1:
+            sole_victors.append(level)
+
+        # add record holder
+        record_holder = leaderboard_data[0]["user_id"]
+        if record_holder not in leaderboard:
+            leaderboard[record_holder] = [0, [], leaderboard_data[0]["user_name"]]
+
+        leaderboard[record_holder][0] += 1
+        leaderboard[record_holder][1].append(
+            [title + "|" + identifier]
+        )  # TODO: why did i add the title here
+
+        # difficulty
+        difficulty_string = (
+            statistics["difficulty_string"]
+            if "difficulty_string" in statistics
+            else "unrated"
+        )
+        difficulty_lengths[difficulty_string] += 1
+
+        # finishes
+        for record in leaderboard_data:
+            user_id = record["user_id"] if "user_id" in record else None
+            timestamp = record["timestamp"] if "timestamp" in record else None
+
+            if user_id is None:
+                continue
+
+            # timestamp data
+            if timestamp is not None:
+                timestamp_id = int(int(timestamp) / 100)
+                timestamps_data.append(f"{user_id}:{timestamp_id}")
+
+            # difficulty records
+            diff_records = difficulty_records[difficulty_string]
+            if user_id not in diff_records:
+                diff_records[user_id] = {
+                    "maps": 0,
+                    "user_name": record["user_name"],
+                }
+            diff_records[user_id]["maps"] += 1
+
+    # first person to complete (in top 100)
+    first_record = None
+    first_timestamp = float('inf') # wtf is this syntax
+
+    # all user stats
+    for record in leaderboard_data:
+        current_timestamp = record["timestamp"]
+        user_id = record["user_id"]
+
+        if int(current_timestamp) < first_timestamp:
+            first_timestamp = int(current_timestamp)
+            first_record = record
+
+        # total finishes
+        if user_id not in user_finishes:
+            user_finishes[user_id] = [0, record["user_name"], 0]
+        user_finishes[user_id][0] += 1
+
+        try:
+            user_finishes[user_id][2] += (
+                record["best_time"] if "best_time" in record else 0
+            )
+        except TypeError: # FIXME: what caused this?
+            print(user_finishes[user_id])
+
+    # first person to complete
+    if first_record is not None:
+        first_user_id = first_record["user_id"]
+        first_user_name = first_record["user_name"]
+
+        if first_user_id not in first_to_beat:
+            first_to_beat[first_user_id] = [first_user_name, 0]
+        first_to_beat[first_user_id][1] += 1
+
+    print(f"Processed {title}")
+
+
+with open("stats_data/all_verified.json") as file:
+    levels = json.load(file)
+
+difficulty_lengths["total"] = len(levels)
+
+# process all data
 with ThreadPoolExecutor() as executor:
-    futures = [executor.submit(process_level, level) for level in data]
+    futures = [executor.submit(process_level, level) for level in levels]
 
     for future in futures:
         future.result()
-        
-leaderboard = {user: scores for user, scores in leaderboard.items() if scores[0] >= 10}
-print("clean leaderboard main")
 
-sorted_leaderboard = dict(sorted(leaderboard.items(), key=lambda x: x[1][0], reverse=True))
-print("sorted leaderboard main")
+# clean, sort, and slice
+leaderboard = {user: scores for user, scores in leaderboard.items() if scores[0] >= 10}
+sorted_leaderboard = dict(
+    sorted(leaderboard.items(), key=lambda x: x[1][0], reverse=True)
+)
 
 for difficulty in difficulty_records:
-    difficulty_records[difficulty] = {user_key: records for user_key, records in difficulty_records[difficulty].items() if records["maps"] >= 10}
-    print("clean leaderboard " + difficulty)
-    
-    difficulty_records[difficulty] = dict(sorted(difficulty_records[difficulty].items(), key=lambda x: x[1]["maps"], reverse=True)[:200])
-    print("sorted leaderboard " + difficulty)
+    difficulty_records[difficulty] = {
+        user_key: records
+        for user_key, records in difficulty_records[difficulty].items()
+        if records["maps"] >= 10
+    }
 
-user_finishes = {key: [finishes[0], finishes[1], round(finishes[2], 2)] for key, finishes in user_finishes.items() if finishes[0] >= 10}
-print("clean leaderboard finishes")
+    difficulty_records[difficulty] = dict(
+        sorted(
+            difficulty_records[difficulty].items(),
+            key=lambda x: x[1]["maps"],
+            reverse=True,
+        )[:200]
+    )
 
-user_finishes = dict(sorted(user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200])
-print("sorted leaderboard finishes")
+user_finishes = {
+    key: [finishes[0], finishes[1], round(finishes[2], 2)]
+    for key, finishes in user_finishes.items()
+    if finishes[0] >= 10
+}
+user_finishes = dict(
+    sorted(user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200]
+)
 
 first_to_beat = {key: beats for key, beats in first_to_beat.items() if beats[1] >= 10}
-print("clean leaderboard first to beat")
+first_to_beat = dict(
+    sorted(first_to_beat.items(), key=lambda x: x[1][1], reverse=True)[:200]
+)
 
-first_to_beat = dict(sorted(first_to_beat.items(), key=lambda x: x[1][1], reverse=True)[:200])
-
-output_files = {
-    "stats_data/user_finishes.json": user_finishes,
-    "stats_data/sorted_leaderboard_records.json": sorted_leaderboard,
-    # "stats_data/leaderboard_levels.json": data,
-    "stats_data/sole_victors.json": sole_victors,
-    "stats_data/difficulty_records.json": difficulty_records,
-    "stats_data/difficulty_lengths.json": difficulty_lengths,
-    "stats_data/first_to_beat.json": first_to_beat,
-    "stats_data/timestamps_data.json": timestamps_data
-}
-
-for filepath, content in output_files.items():
-    with open(filepath, "w") as file:
-        json.dump(content, file)
-
-print("All data saved successfully")
+save("user_finishes", user_finishes)
+save("sorted_leaderboard_records", sorted_leaderboard)
+save("sole_victors", sole_victors)
+save("difficulty_records", difficulty_records)
+save("difficulty_lengths", difficulty_lengths)
+save("first_to_beat", first_to_beat)
+save("timestamps_data", timestamps_data)
